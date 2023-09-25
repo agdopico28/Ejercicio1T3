b17c580f3a18011c4c030421b83b2775ef47bbfe
amali <alugar1951@ieselcaminas.oreg>
2023-09-25 13:18:02 +0200

    casa commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,21 @@ package exemples
 
 import java.io.DataInputStream
 import java.io.FileInputStream
-import java.io.RandomAccessFile
+import java.sql.DriverManager.println
 
 fun main(args: Array<String>) {
-    val f =  DataInputStream(FileInputStream("Empleats.dat"))
+    val f = DataInputStream(FileInputStream("Rutes.dat"))
+
     while (f.available() > 0) {
-    println("Nom: " + f.readUTF())
-    println("Desnivell.: " + f.readInt())
-    println("Desnivell acumulat: " + f.readInt())
-    println("Numero de punts: " + f.readInt())
-    println("Punts: " + f.readInt())
-    f.close()
+        System.out.println("Ruta: " + f.readUTF())
+        System.out.println("Desnivell: " + f.readInt())
+        System.out.println("DesnivellAcumulat: " + f.readInt())
+        var punts = f.readInt()
+        System.out.println("Té $punts punts")
+        for(i in 1..punts){
+           System.out.println("Punt $i: " + f.readUTF() + "(" + f.readDouble()+ "," + f.readDouble() + ")")
+        }
     }
+    f.close()
 }
 
